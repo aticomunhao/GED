1339f4018895b3ba073e0768562efe64ba586acb
Wescley Lima <wescley.lima@engesoftware.com.br>
2025-06-03 17:51:11 -0300

    Correção data hora e usuário retirada estoque

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,6 +83,7 @@ $(document).ready(function () {
 
                                 $('.informacoes').show().html("\
                                 <h2>\
+                                <input name=\"id_usuario\" type=\"hidden\" value=\"" + data[prop].id + "\"\
                                     <strong>Dados do Usuário</strong></h2>\n\
                                     <strong>Nome: </strong>" + data[prop].nome + "<br>\n\
                                     <strong>Cidade/Estado: \n\
